2c2fe7e7e15bfaf677ecbcc2e8c94d77a7774651
guy <guy@localhost.localdomain>
2014-11-18 20:09:19 +0100

    Construction d une base claire

--- src/DefaultPackage/Lea.java
+++ src/DefaultPackage/Lea.java
@@ -6,44 +6,62 @@ import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 public class Lea {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         ConfigurationManager cm;
         cm = new ConfigurationManager(Lea.class.getResource("Lea.sphinx4.config.xml"));
-       
+        												// acces au fichier de config
 
         Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-        recognizer.allocate();
-
-        // start the microphone or exit if the programm if this is not possible
+        recognizer.allocate();							// allocation du moteur de
+        												// reconnaissance vocal
+        											
+        
         Microphone microphone = (Microphone) cm.lookup("microphone");
-        if (!microphone.startRecording()) {
+        
+        if (!microphone.startRecording())
+        {
             System.out.println("Impossible de démarrer le micro.");
             recognizer.deallocate();
             System.exit(1);
         }
 
-        System.out.println("Lea vous écoute : ");
-        fonction.say("Bonjour");
-        // loop the recognition until the programm exits.
-        while (true) {
-            System.out.println("Vous pouvez parler ou écrire :\n");
+        
+        
+        
+        
+        
+        System.out.println("Lea vous écoute : ");			
+        fonction.say("Bonjour");							// pour les tests
+        
+//=====================Grande Boucle================================
+        while (true)
+        {
+            System.out.println("Vous pouvez parler :\n");
             
             Result result = recognizer.recognize();
 
-            if (result != null) {
+            if (result != null)
+            {
+            	
                 String resultText = result.getBestFinalResultNoFiller();
-                System.out.println("Vous avez dit: " + resultText);
                 
-                if (resultText.contains("bonjour")) {
-                	fonction.say("Bonjour");
-            }
+                System.out.println("Vous avez dit : " + resultText);
                 
-            } else {
+                if (resultText.contains("bonjour"))
+                {
+                	fonction.say("Bonjour enculé de merde!");
+                }
+                
+            }else
+            {
                 System.out.println("Je n'ai pas entendu ce que vous avez dit .\n");
             }
             
             
             
-        }
-    }
+        }// END while(true)
+        
+    }// END main
+    
 }
--- src/DefaultPackage/fonction.java
+++ src/DefaultPackage/fonction.java
@@ -1,24 +1,23 @@
 package DefaultPackage;
 
 import java.io.IOException;
-
 import com.developpez.adiguba.shell.Shell;
 
 public class fonction {
-	public static void say(String text) {
+	
+	public static void say(String text)
+	{
 		Shell sh = new Shell();
-		try {
-			String create_sound = "pico2wave -l fr-FR  -w reponse.wav " + text;
-			String play_sound = "play reponse.wav";
-			String result = sh.command(create_sound).consumeAsString();
-			//System.out.println(result);
-			String play = sh.command(play_sound).consumeAsString();
-			//System.out.println(play);
-			String delete_file = sh.command("rm reponse.wav").consumeAsString();
-			//System.out.println(delete_file);
+		
+		try
+		{
+			sh.command("pico2wave -l fr-FR  -w reponse.wav '" + text + "'").consumeAsString();
+			sh.command("play reponse.wav").consumeAsString();
+			sh.command("rm reponse.wav").consumeAsString();
 			
-		} catch (IllegalStateException | IOException e) {
+		}catch (IOException e)
+		{
 
-		}
-	}
-}
+		} // END try...catch
+	}// END say(String text)
+}// END fonction class
